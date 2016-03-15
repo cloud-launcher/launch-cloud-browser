d37dcfa65e297d5623f501b3481d24ee0d3bf6bd
Blake La Pierre <blakelapierre@gmail.com>
2016-03-14 23:07:35 -0700

    Update .dist

--- .dist/index.js
+++ .dist/index.js
@@ -5,11 +5,12 @@ var $__launch_45_cloud_45_core__,
 var core = ($__launch_45_cloud_45_core__ = require("launch-cloud-core"), $__launch_45_cloud_45_core__ && $__launch_45_cloud_45_core__.__esModule && $__launch_45_cloud_45_core__ || {default: $__launch_45_cloud_45_core__}).default;
 var digitalocean = ($__provider_45_digitalocean_47__46_dist_47_provider_47_browser__ = require("provider-digitalocean/.dist/provider/browser"), $__provider_45_digitalocean_47__46_dist_47_provider_47_browser__ && $__provider_45_digitalocean_47__46_dist_47_provider_47_browser__.__esModule && $__provider_45_digitalocean_47__46_dist_47_provider_47_browser__ || {default: $__provider_45_digitalocean_47__46_dist_47_provider_47_browser__}).default;
 var request = ($__browser_45_request__ = require("browser-request"), $__browser_45_request__ && $__browser_45_request__.__esModule && $__browser_45_request__ || {default: $__browser_45_request__}).default;
+var ip = '104.236.140.79';
 var proxies = {
-  dockerHubApiRoot: 'http://104.131.153.23:8082',
-  discoveryEtcdApiRoot: 'http://104.131.153.23:8081'
+  dockerHubApiRoot: ("http://" + ip + ":8082"),
+  discoveryEtcdApiRoot: ("http://" + ip + ":8081")
 };
-module.exports = (function(providerConfigs, log) {
+module.exports = function(providerConfigs, log) {
   if (providerConfigs === undefined || providerConfigs === null)
     throw Error('Must provide a providerConfigs object!');
   var launchCloud = core({
@@ -20,6 +21,6 @@ module.exports = (function(providerConfigs, log) {
     proxies: proxies
   });
   return launchCloud;
-});
+};
 
 //# sourceMappingURL=index.js.map
